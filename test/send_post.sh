ab43536b0cf45fd25d4c51cd0d353ca2259045bd
Oren Oichman <ooichman@ooichman.redhat.com>
2021-11-19 21:01:58 +0200

    fix deployment

--- test/send_post.sh
+++ test/send_post.sh
@@ -10,6 +10,11 @@
 		exit
    fi
 
+   if [[ -z ${DST_PORT} ]]; then
+   	echo "The variable DST_PORT is not defined"
+   	exit
+   fi
+
    if [[ -z ${OCP_HOSTNAME} ]]; then
 		echo "the variable OCP_HOSTNAME is not defined"
 		exit
@@ -18,7 +23,8 @@
 		JSONstring=$( jq -n \
 			--arg ru "${REMOTE_URL}" \
 			--arg oh "${OCP_HOSTNAME}" \
-			'{"port":"443","target": $ru , "protocol": "tcp" , "hostname", $oh}' )
+			--arg rp "${DST_PORT}" \
+			'{"port": $rp ,"target": $ru , "protocol": "tcp" , "hostname", $oh}' )
 
       
       curl -H "Content-type: application/json" -X POST -d "$JSONstring" $MY_URL
